6aa2010e0470d3261129fa68718b234f9bb275ca
joolas <joonas.lassila@gmail.com>
2012-05-03 10:15:26 +0300

    Update imagesrc.php

--- imagesrc.php
+++ imagesrc.php
@@ -2,7 +2,7 @@
   $img_id = $_GET['img_id'];
 	if(isset($img_id) && is_numeric($img_id))
 		{
-			include("../con/update_db.php");
+			include("con_db.php");
 			$imageQuery = "SELECT image, image_type FROM images WHERE image_id=$img_id";
 			$queryResult = mysql_query($imageQuery) or die("Invalid query: " . mysql_error());
 			if(mysql_num_rows($queryResult) > 0)
@@ -13,7 +13,5 @@
 				header($headerString);
 				echo mysql_result($queryResult, 0, "image");
 			}
-			// close the db link
-			mysql_close($con);
 		}
 ?>
