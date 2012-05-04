9c5db2f1a09c6234e85a0f60782678c315fdb1e3
joolas <joonas.lassila@gmail.com>
2012-05-04 09:48:05 +0300

    Update gen_update.php

--- gen_update.php
+++ gen_update.php
@@ -1,2 +1,20 @@
-<div id="gen_update">
-</div>
+<?php
+  include("config.php");
+  include("con_db.php");
+  $header = $_POST['header'];
+  $content = $_POST['content'];
+  $author = $_POST['author'];
+  $id = $_POST['article_id'];
+  $sql = "UPDATE gen_info SET header='$header', content='$content', author='$author' WHERE article_id='$id'";
+	$result = mysql_query($sql);
+	if ($result)
+		{
+			echo "<meta http-equiv=\"refresh\" content=\"0;URL=index.php?id=10\">";
+		}
+		
+	else 
+		{
+			echo "ERROR";
+		}
+	mysql_close($con)
+?>
