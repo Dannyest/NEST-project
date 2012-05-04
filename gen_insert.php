53792120c85abdff166ca69158fdf5ed9b87a052
joolas <joonas.lassila@gmail.com>
2012-05-04 09:36:21 +0300

    Update gen_insert.php

--- gen_insert.php
+++ gen_insert.php
@@ -1,2 +1,23 @@
-<div id="gen_insert">
-</div>
+<?php
+  include("config.php");  
+  include("con_db.php");
+  $siteid = $_POST['site_id'];
+  $header = $_POST['header'];
+  $content = $_POST['content'];
+	$author = $_POST['author'];
+  
+	$sql = "INSERT INTO gen_info (site_id, header, content, author) VALUES ('$siteid', '$header', '$content', '$author')";
+	$result = mysql_query($sql);
+
+
+
+  if($result)
+	{
+		echo "<meta http-equiv=\"refresh\" content=\"0;URL=index.php?id=11\">";
+	}
+
+	else 
+	{
+		echo "ERROR";
+	}	
+?>
