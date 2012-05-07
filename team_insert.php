ec79eae094f3e0defbba607334a3b5d95ed1013e
Dannyest <Daniel.Rantala@gmail.com>
2012-05-07 10:23:56 +0300

    Update team_insert.php

--- team_insert.php
+++ team_insert.php
@@ -4,38 +4,24 @@
   	$name = $_POST['person_name'];
 	$desc = $_POST['person_desc'];
 	$status = $_POST['status'];
-	$sql = "INSERT INTO team (person_name, person_desc, status) VALUES ('$name', '$desc', '$status')";
-	$result = mysql_query($sql);
-	
-  if(isset($_FILES['imagefile']))
+
+	  if(isset($_FILES['imagefile']))
 	{
-		try
-			{
+		
 				$imgData = addslashes(file_get_contents($_FILES['imagefile']['tmp_name']));
 				$size = getimagesize($_FILES['imagefile']['tmp_name']);
-				$imageSql = "INSERT INTO team (person_img) VALUES ('{$_FILES['imagefile']['name']}')";
+				$imageSql = "INSERT INTO team (person_name, person_desc, status, person_img) VALUES ('$name', '$desc', '$status', '$imgData')";
 				
+
         if(!mysql_query($imageSql))
 				{
 					echo 'Upload failed';
 				}
 				
-			
-      catch(Exception $e) 
-			{
-				echo $e->getMessage();
-				echo "Problem loading the file";
-			}
-  		}
 	}
-	
-  if($result)
-	{
-		echo "<meta http-equiv=\"refresh\" content=\"0;URL=index.php?id=10\">";
-	}
+else   {
+      		     		$imageSql = "INSERT INTO team (person_name, person_desc, status) VALUES ('$name', '$desc', '$status')";
 
-	else 
-	{
-		echo "ERROR";
-	}	
+      		}			
+	header("Location: index.php?id=10");
 ?>
