e848007c1684ae91d9b97f9b1e50129aea081cdf
tkemppainen <r1kete00@students.oamk.fi>
2012-05-08 10:02:01 +0300

    Update gen_insert.php

--- gen_insert.php
+++ gen_insert.php
@@ -1,10 +1,11 @@
 <?php
   include("config.php");	
   include("con_db.php");
+  	$site_id = $_POST['site_id'];
   	$header = $_POST['header'];
 	$content = $_POST['content'];
 	$author = $_POST['author'];
-       $imgname = ($_FILES["file"]["name"]);
+      	$imgname = ($_FILES["file"]["name"]);
 	 if ((($_FILES["file"]["type"] == "image/gif")
 || ($_FILES["file"]["type"] == "image/jpeg")
 || ($_FILES["file"]["type"] == "image/png")
@@ -25,7 +26,7 @@
       {
       move_uploaded_file($_FILES["file"]["tmp_name"],
       "/home/user/public_html/NEST/images/" . $_FILES["file"]["name"]);
-	mysql_query("INSERT INTO team (image_name, header, content, author) VALUES ('$imgname', '$header', '$content', '$author')");	
+	mysql_query("INSERT INTO team (site_id, image_name, header, content, author) VALUES ('$site_id', '$imgname', '$header', '$content', '$author')");	
       }
     }
   }
