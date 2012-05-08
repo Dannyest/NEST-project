8d27455cd3517edcbc378888cb4d43ec0dc6b4e8
tkemppainen <r1kete00@students.oamk.fi>
2012-05-08 09:58:13 +0300

    Update gen_insert.php

--- gen_insert.php
+++ gen_insert.php
@@ -1,23 +1,37 @@
 <?php
-  include("config.php");  
+  include("config.php");	
   include("con_db.php");
-  $siteid = $_POST['site_id'];
-  $header = $_POST['header'];
-  $content = $_POST['content'];
+  	$header = $_POST['header'];
+	$content = $_POST['content'];
 	$author = $_POST['author'];
-  
-	$sql = "INSERT INTO gen_info (site_id, header, content, author) VALUES ('$siteid', '$header', '$content', '$author')";
-	$result = mysql_query($sql);
-
-
-
-  if($result)
-	{
-		echo "<meta http-equiv=\"refresh\" content=\"0;URL=index.php?id=11\">";
-	}
-
-	else 
-	{
-		echo "ERROR";
-	}	
+       $imgname = ($_FILES["file"]["name"]);
+	 if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/png")
+|| ($_FILES["file"]["type"] == "image/pjpeg"))
+&& ($_FILES["file"]["size"] < 20000000))
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+    }
+  else
+    {
+    if (file_exists("/home/user/public_html/NEST/images/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "/home/user/public_html/NEST/images/" . $_FILES["file"]["name"]);
+	mysql_query("INSERT INTO team (image_name, header, content, author) VALUES ('$imgname', '$header', '$content', '$author')");	
+      }
+    }
+  }
+else
+  {
+  echo "Invalid file";
+  }
+	header("Location: index.php?id=11");
 ?>
