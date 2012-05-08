9f0b2ad3bcbe0868fcc9a33203fa4fc4f52ba314
tkemppainen <r1kete00@students.oamk.fi>
2012-05-08 09:52:47 +0300

    Update wos_cms.php

--- wos_cms.php
+++ wos_cms.php
@@ -1,8 +1,8 @@
 <div id="wos_cms">
 
    <?php 
-      include("cms_menu.php");
-      include("config.php");
+      	include("cms_menu.php");
+      	include("config.php");
     	include("con_db.php");
  	
     	$query = "SELECT * FROM gen_info WHERE site_id=1 ORDER BY article_id DESC"; 
@@ -10,19 +10,21 @@
 
     	$rowCount = mysql_numrows($queryResult);
 
+  		$action = $_GET['action']; 
+	    	$ud_id = $_GET['ud_id'];
+
 
     for($i = 0; $i < $rowCount; $i++) 
 	  	{ 
 	  		$author = mysql_result($queryResult, $i, "author"); 
 		  	$header = mysql_result($queryResult, $i, "header"); 
-			  $content = mysql_result($queryResult, $i, "content"); 
-  			$img = mysql_result($queryResult, $i, "img"); 
+			$content = mysql_result($queryResult, $i, "content"); 
 	  		$id = mysql_result($queryResult, $i, "article_id"); 
 		  	echo '<div>';
 		  	echo $header;
 		  	echo $author;
 		  	echo $content;
-		  	echo $img;
+		  	echo "<img src=http://bubble.ipt.oamk.fi/NEST/images/$img>";
 		  	echo "<a href='index.php?id=11&action=edit&ud_id=$id'>Edit</a>";
 		  	echo "<a href='index.php?id=11&action=delete&ud_id=$id'>Delete</a>";
 		  	echo '</div>';
@@ -30,8 +32,7 @@
 
     	echo '<a href="index.php?id=11&action=insert">Insert</a>';
 
-      $action = $_GET['action']; 
-	    $ud_id = $_GET['ud_id'];
+  
 
   		if($action == "edit")
   	  	  { 	    	
@@ -39,19 +40,19 @@
 	 	  $selectPost = @mysql_fetch_array(@mysql_query("SELECT * FROM gen_info WHERE article_id='$ud_id'"));
 
 		  ?>
-		  <input name="article_id" type="hidden" value="<?php echo $selectPost['article_id'];?>">
-		 	<form action="index.php?id=11" method="post" name="post">
-			<p>Post Name:<br />
+		 	<form action="gen_update.php" method="post" name="post">
+		     	<input name="article_id" type="hidden" value="<?php echo $selectPost['article_id'];?>">
+			<p>Header:<br />
 			<input name="header" type="text"value="<?php echo $selectPost['header'];?>" size="45" />
-		  </p>
-		  <p>Post Status:<br />
+		  	</p>
+		   	<p>Author:<br />
 			<input name="author" type="text"value="<?php echo $selectPost['author'];?>" size="45" />
-		  </p>
-		  <p>Post Content:<br />
+		  	</p>
+		  	<p>Content:<br />
 			<textarea name="content" cols="40" rows="15"><?php echo $selectPost['content'];?></textarea>
-		  </p>
-		  <input name="update" type="submit" value="Update" />
-  	   </form>
+		    	</p>
+		    	<input name="update" type="submit" value="Update" />
+		    	</form>
 	
 <?php 
   	    }
@@ -59,19 +60,18 @@
 		if($action == "insert")
 		   { 
 		   ?>
-		   	<input name="site_id" type="hidden" value="1">
 			<form enctype="multipart/form-data" action="gen_insert.php" method="POST">
-			<div>
-			<div>Header</div>
-			<div><input type="text" size="40" name="header" value="" /></div>
-			</div>
-		
 			<div>
 			<div>Author</div>
 			<div><input type="text" size="40" name="author" value="" /></div>
+			</div>
+		
+			<div>
+			<div>Header</div>
+			<div><input type="text" size="40" name="header" value="" /></div>
 			<div>
 				<div>Image</div>
-				<div><input name="imagefile" type="file" /></div>
+				<div><input name="file" type="file" id="file"/></div>
 			</div>
 			</div>
 			<div>
